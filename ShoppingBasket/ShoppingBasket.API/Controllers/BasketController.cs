b771b58b7833df37bfc3e279c742024984968aa6
Pedro Diaz <PedroDiaz@criticalmanufacturing.com>
2025-03-13 21:09:31 +0000

    Project Creation.

---
+++ ShoppingBasket/ShoppingBasket.API/Controllers/BasketController.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace ShoppingBasket.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class BasketController : ControllerBase
+    {
+    }
+}
---
+++ ShoppingBasket/ShoppingBasket.Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace ShoppingBasket.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
